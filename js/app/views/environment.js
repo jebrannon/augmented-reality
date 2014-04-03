e3f5a225fee9130ce095c7c08378e525a96a2b46
James Brannon <me@jamesbrannon.co.uk>
2014-04-03 17:55:24 +0100

    fixed variable names

--- js/app/views/environment.js
+++ js/app/views/environment.js
@@ -25,27 +25,27 @@ define([
 			  this._recall.touch.y = false;
 
 			  //  Image
-			  this.image = {};
-			  this.image.touchSensitivity = 1;
-			  this.image.giroSensitivity = 1;
-			  this.image.el = $(this.el).find('#panorama')[0];
-			  this.image.offset = {};
-			  this.image.offset.width = this.image.el.offsetWidth - this.el.offsetWidth;
-			  this.image.offset.height = this.image.el.offsetHeight- this.el.offsetHeight;
-			  this.image.ratio = {};
-			  this.image.ratio.z = Math.abs(this.image.offset.width / 360);
-			  this.image.ratio.y = Math.abs(this.image.offset.height / 180);
-			  this.image.orig = {};
-			  this.image.orig.x = this.image.el.offsetLeft;
-			  this.image.orig.y = this.image.el.offsetTop;
-			  this.image.now = {};
-			  this.image.now.x = 0;
-			  this.image.now.y = 0;
-			  this.image.limit = {};
-			  this.image.limit.left = Math.abs(this.image.orig.x);
-			  this.image.limit.right = -(this.image.offset.width - Math.abs(this.image.orig.x));
-			  this.image.limit.top = Math.abs(this.image.orig.y);
-			  this.image.limit.bottom = -(this.image.offset.height - Math.abs(this.image.orig.y));
+			  this._image = {};
+			  this._image.touchSensitivity = 1;
+			  this._image.giroSensitivity = 1;
+			  this._image.el = $(this.el).find('#panorama')[0];
+			  this._image.offset = {};
+			  this._image.offset.width = this._image.el.offsetWidth - this.el.offsetWidth;
+			  this._image.offset.height = this._image.el.offsetHeight- this.el.offsetHeight;
+			  this._image.ratio = {};
+			  this._image.ratio.z = Math.abs(this._image.offset.width / 360);
+			  this._image.ratio.y = Math.abs(this._image.offset.height / 180);
+			  this._image.orig = {};
+			  this._image.orig.x = this._image.el.offsetLeft;
+			  this._image.orig.y = this._image.el.offsetTop;
+			  this._image.now = {};
+			  this._image.now.x = 0;
+			  this._image.now.y = 0;
+			  this._image.limit = {};
+			  this._image.limit.left = Math.abs(this._image.orig.x);
+			  this._image.limit.right = -(this._image.offset.width - Math.abs(this._image.orig.x));
+			  this._image.limit.top = Math.abs(this._image.orig.y);
+			  this._image.limit.bottom = -(this._image.offset.height - Math.abs(this._image.orig.y));
 
 			  // 
 			  var that = this;
@@ -125,43 +125,43 @@ define([
 				}
 			},
 			updatePanoramaPositionGiro: function(angleZ, angleY) {
-			  if (!this.recall.giro.z) {
-			   this.recall.giro.z = angleZ;
+			  if (!this._recall.giro.z) {
+			   this._recall.giro.z = angleZ;
 			  }
-			  if (!this.recall.giro.y) {
-			    this.recall.giro.y = angleY;
+			  if (!this._recall.giro.y) {
+			    this._recall.giro.y = angleY;
 			  }
-			  var diffX = Math.abs(angleZ - this.recall.giro.z) > 100 ? 0 : angleZ - this.recall.giro.z;
-			  var diffY = angleY - this.recall.giro.y;
-			  var newX = Math.round(this.image.now.x + (diffX * this.image.ratio.z));
-			  var newY = Math.round(this.image.now.y + ((diffY * this.image.ratio.y) * this.image.giroSensitivity));
+			  var diffX = Math.abs(angleZ - this._recall.giro.z) > 100 ? 0 : angleZ - this._recall.giro.z;
+			  var diffY = angleY - this._recall.giro.y;
+			  var newX = Math.round(this._image.now.x + (diffX * this._image.ratio.z));
+			  var newY = Math.round(this._image.now.y + ((diffY * this._image.ratio.y) * this._image.giroSensitivity));
 			  var translate = 'translate(';
 
 			  //  X position
-			  if (newX < this.image.limit.left && newX > this.image.limit.right) {
-			    this.image.now.x = newX;
+			  if (newX < this._image.limit.left && newX > this._image.limit.right) {
+			    this._image.now.x = newX;
 			    translate += newX + 'px,';
 			  } else {
-			    translate += this.image.now.x + 'px,';
+			    translate += this._image.now.x + 'px,';
 			  }
 
 			  //  Y position
-			  if (newY < this.image.limit.top && newY > this.image.limit.bottom) {
-			    this.image.now.y = newY;
+			  if (newY < this._image.limit.top && newY > this._image.limit.bottom) {
+			    this._image.now.y = newY;
 			    translate += newY + 'px';
 			  } else {
-			    translate += this.image.now.y + 'px';
+			    translate += this._image.now.y + 'px';
 			  }
 
 			  //  update event
 			  this._delegate[GyroPanorama.DID_PAN].call(this._delegate, {diffX: Math.abs(diffX), diffY: Math.abs(diffY) });
 
 			  //  close and update
-			  this.image.el.style.webkitTransform = translate + ')';
+			  this._image.el.style.webkitTransform = translate + ')';
 
 			  //  record for 'diff' calculation
-			  this.recall.giro.z = angleZ;
-			  this.recall.giro.y = angleY;
+			  this._recall.giro.z = angleZ;
+			  this._recall.giro.y = angleY;
 			},
 			output: function() {
 				// $('#loading').hide();
