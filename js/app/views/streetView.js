bfd375aba3baf32513089c30a76a8ac7c011b605
James Brannon <me@jamesbrannon.co.uk>
2014-04-13 19:17:38 +0100

    map event integration into backbone structure

--- js/app/views/streetView.js
+++ js/app/views/streetView.js
@@ -34,32 +34,23 @@ define([
 				};
 				var panorama = new google.maps.StreetViewPanorama(this.el, options);
 
+				//  Map listeners
 				google.maps.event.addListener(panorama, 'pano_changed', function() {
-					that._location.set('panid', panorama.getPano());
-				  that.output();
+					that.handleMapEvent({type: 'pano_changed', target: panorama});
 				});
 
 				google.maps.event.addListener(panorama, 'links_changed', function() {
-				      // var linksTable = document.getElementById('links_list');
-				      // while(linksTable.hasChildNodes()) {
-				      //   linksTable.removeChild(linksTable.lastChild);
-				      // };
-				      // var links =  panorama.getLinks();
-				      // for (var i in links) {
-				      //   var row = document.createElement('tr');
-				      //   linksTable.appendChild(row);
-				      //   var labelCell = document.createElement('td');
-				      //   labelCell.innerHTML = '<b>Link: ' + i + '</b>';
-				      //   var valueCell = document.createElement('td');
-				      //   valueCell.innerHTML = links[i].description;
-				      //   linksTable.appendChild(labelCell);
-				      //   linksTable.appendChild(valueCell);
-				      // }
+					that.handleMapEvent({type: 'links_changed', target: panorama});
 				});
 
 				google.maps.event.addListener(panorama, 'position_changed', function() {
-				      // var positionCell = document.getElementById('position_cell');
-				      // positionCell.firstChild.nodeValue = panorama.getPosition() + '';
+					that.handleMapEvent({type: 'position_changed', target: panorama});
+				});
+
+				google.maps.event.addListener(panorama, 'pov_changed', function() {
+					that.handleMapEvent({type: 'pov_changed', target: panorama});
+				});
+
 
 				      // document.getElementById('button').addEventListener('click', function() {
 				      // 	console.log('clicked');
@@ -68,13 +59,6 @@ define([
 								  //   pitch:50}
 								  // );
 				      // })
-				});
-
-				google.maps.event.addListener(panorama, 'pov_changed', function() {
-					that._location.set('heading', panorama.getPov().heading);
-					that._location.set('pitch', panorama.getPov().pitch);
-					that.output();
-				});
 			},
 			handleDeviceOrientation: function(event) {
 				console.log('gamma', event.gamma);
@@ -82,91 +66,41 @@ define([
 				console.log('alpha', event.alpha);
 				console.log('----');
 			},
-			handleEvent: function(e) {
-				
-				// console.log('e);
-				if (e.target && e.target.getAttribute('data-app-action')) {
-
-
-
-					// if (!this._isFullscreen) {
-
-					//  if(this.el.requestFullscreen) {
-					//     this.el.requestFullscreen();
-					//     this._isFullscreen = true;
-					//     console.log('requestFullscreen');
-					//   } else if(this.el.mozRequestFullScreen) {
-					//     this.el.mozRequestFullScreen();
-					//     this._isFullscreen = true;
-					//     console.log('mozRequestFullScreen');
-					//   } else if(this.el.webkitRequestFullscreen) {
-					//     this.el.webkitRequestFullscreen();
-					//     this._isFullscreen = true;
-					//     console.log('webkitRequestFullscreen');
-					//   } else if(this.el.msRequestFullscreen) {
-					//     this.el.msRequestFullscreen();
-					//     this._isFullscreen = true;
-					//     console.log('msRequestFullscreen');
-					//   } else {
-					//   	console.log('unsupported!!!');
-					//   }
-
-					// }
-					// else {
-					//  if(this.el.exitFullscreen) {
-					//     this.el.exitFullscreen();
-					//     this._isFullscreen = false;
-					//     console.log('exitFullscreen');
-					//   } else if(this.el.mozCancelFullScreen) {
-					//     this.el.mozCancelFullScreen();
-					//     this._isFullscreen = false;
-					//     console.log('mozCancelFullScreen');
-					//   } else if(this.el.webkitExitFullscreen) {
-					//     this.el.webkitExitFullscreen();
-					//     this._isFullscreen = false;
-					//     console.log('webkitExitFullscreen');
-					//   } else {
-					//   	console.log('unsupported!!!');
-					//   }
-					// }
-					
+			handleMapEvent: function(event) {
+				switch(event.type) {
+					case 'pano_changed':
+						this._location.set('panid', event.target.getPano());
+						this.output()
+					  break;
+					case 'links_changed':
+					      // var linksTable = document.getElementById('links_list');
+					      // while(linksTable.hasChildNodes()) {
+					      //   linksTable.removeChild(linksTable.lastChild);
+					      // };
+					      // var links =  panorama.getLinks();
+					      // for (var i in links) {
+					      //   var row = document.createElement('tr');
+					      //   linksTable.appendChild(row);
+					      //   var labelCell = document.createElement('td');
+					      //   labelCell.innerHTML = '<b>Link: ' + i + '</b>';
+					      //   var valueCell = document.createElement('td');
+					      //   valueCell.innerHTML = links[i].description;
+					      //   linksTable.appendChild(labelCell);
+					      //   linksTable.appendChild(valueCell);
+					      // }
+					  break;
+					case 'position_changed':
+					  break;
+					case 'pov_changed':
+							this._location.set('heading', event.target.getPov().heading);
+							this._location.set('pitch', event.target.getPov().pitch);
+							this.output()
+					  break;
 				}
 			},
-			updatePanoramaPositionGiro: function(angleZ, angleY) {
-			  if (!this._recall.giro.z) {
-			   this._recall.giro.z = angleZ;
-			  }
-			  if (!this._recall.giro.y) {
-			    this._recall.giro.y = angleY;
-			  }
-			  var diffX = Math.abs(angleZ - this._recall.giro.z) > 100 ? 0 : angleZ - this._recall.giro.z;
-			  var diffY = angleY - this._recall.giro.y;
-			  var newX = Math.round(this._image.now.x + (diffX * this._image.ratio.z));
-			  var newY = Math.round(this._image.now.y + ((diffY * this._image.ratio.y) * this._image.giroSensitivity));
-			  var translate = 'translate(';
-
-			  //  X position
-			  if (newX < this._image.limit.left && newX > this._image.limit.right) {
-			    this._image.now.x = newX;
-			    translate += newX + 'px,';
-			  } else {
-			    translate += this._image.now.x + 'px,';
-			  }
-
-			  //  Y position
-			  if (newY < this._image.limit.top && newY > this._image.limit.bottom) {
-			    this._image.now.y = newY;
-			    translate += newY + 'px';
-			  } else {
-			    translate += this._image.now.y + 'px';
-			  }
-
-			  //  close and update
-			  this._image.el.style.webkitTransform = translate + ')';
-
-			  //  record for 'diff' calculation
-			  this._recall.giro.z = angleZ;
-			  this._recall.giro.y = angleY;
+			handleEvent: function(e) {
+				
+				console.log(e);
 			},
 			output: function() {
 				// $('#loading').hide();
